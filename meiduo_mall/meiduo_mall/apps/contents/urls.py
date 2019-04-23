bb8ac8cb3c0aa8ecd5f7638afbe77c803ffc57b0
laura <281633826@qq.com>
2019-04-23 16:58:01 +0800

    用户注册

--- meiduo_mall/meiduo_mall/apps/contents/urls.py
+++ meiduo_mall/meiduo_mall/apps/contents/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from .views import Index
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include("users.urls", namespace="users")),
-    url(r'^', include("contents.urls", namespace="contents")),
-
+    url(r'^$', Index.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,8 +1,54 @@
-from django.shortcuts import render
-from django.views import View
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.http import HttpResponseForbidden
+from django.contrib.auth import login
+import re
+from .models import User
+from django.db import DatabaseError
+import logging
 # Create your views here.
+logger = logging.getLogger('django')
+
 
 class Register(View):
     def get(self, request):
         return render(request, "register.html")
+
+    
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        mobile = request.POST.get('mobile')    
+        sms_code = request.POST.get('sms_code')
+        allow = request.POST.get('allow')
+        if not all([username, password, password2, mobile, sms_code, allow]):
+            return HttpResponseForbidden('输入不能为空')
+
+        if not re.match(r'^[a-zA-Z0-9_-]{5, 20}$', username): 
+            return HttpResponseForbidden('请输入5-20的字符串')
+
+        if not re.match(r'^[a-zA-Z0-9]{8, 20}$', psaaword):
+            return HttpResponseForbidden('请输入8-20位的密码')
+
+        if password2 != password:
+            return HttpResponseForbidden('两次密码不相同')
+
+        if not re.match(r'^1[3456789]\d{9}$', mobile):
+            return HttpResponseForbidden('请输入有效手机号')
+        try:
+            user = User.objects.create_user(
+                    
+                    username = username,
+                    mobile = mobile, 
+                    password = password
+                   
+                    )
+        except DatabaseError as e:
+            
+            logger.error(e)
+            return render(request, 'register.html', {'register_errmsg': '用户注册失败'})
+
+        login(request, user)
+        return redirect('/')
 
